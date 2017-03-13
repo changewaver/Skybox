e848490a1ae6e2f9ed1e173fcda8621e7e753c62
changewaver <changewaver@gmail.com>
2017-03-13 11:07:07 +0800

    Skybox in unity3d changed by time going
    收录了由15个天空球组成的渐变天空球,建立脚本使得天空球按时间发生变化.

---
+++ Skybox_unity/Assets/Cshrp/ReadMe.txt
@@ -0,0 +1,7 @@
+#Skychange{
+	Change Interval 是天空盒的更换速度,取1是一秒每张,取2是两秒每张;
+	Materials.Size 是天空盒的数量,如果取16就可以选取16个天空盒子;
+	Materials.Element 0-16 是天空盒,请选中天空盒;
+	Suggetion: 建议导入足够多的天空盒子后使用,拖拽到某一个静态物体上,比如新建一个cube;
+}
+
---
+++ Skybox_unity/Assets/Cshrp/Skychange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skychange : MonoBehaviour {
+    //public Material[] materials;
+    public float changeInterval = 0.33F;
+    //public Renderer rend;
+    public Material[] materials;
+
+    void Start()
+
+    {
+        int i = 0;
+        //rend = GetComponent<Renderer>();
+        //rend.enabled = true;
+        
+    }
+
+    void Update()
+    {
+        
+        if (materials.Length == 0)
+            return;
+
+        // we want this material index now
+        int index = Mathf.FloorToInt(Time.time / changeInterval);
+
+        // take a modulo with materials count so that animation repeats
+        index = index % materials.Length;
+
+        // assign it to the renderer
+        //rend.sharedMaterial = materials[index];
+
+        
+        RenderSettings.skybox = materials[index];
+    }
+}
